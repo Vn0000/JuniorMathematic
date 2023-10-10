42421307af082d3a5a3956935ea9fc190bce9104
Vn0000 <workmailkonovalov@gmail.com>
2023-10-10 10:18:21 +0400

    Share project on github

---
+++ app/src/main/java/com/bignerdranch/android/juniormathematic/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.juniormathematic.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bignerdranch.android.juniormathematic.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
