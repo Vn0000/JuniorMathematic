0daa2b695d56430ab9d06a8fa431b3ab6099bb2c
Vn0000 <workmailkonovalov@gmail.com>
2023-10-11 14:29:10 +0400

    Добавлена навигация между фрагментами.

--- app/src/main/java/com/bignerdranch/android/juniormathematic/domain/entity/GameResult.kt
+++ app/src/main/java/com/bignerdranch/android/juniormathematic/domain/entity/GameResult.kt
@@ -1,8 +1,10 @@
 package com.bignerdranch.android.juniormathematic.domain.entity
 
+import java.io.Serializable
+
 data class GameResult(
     val winner: Boolean,
     val countOfRightAnswers: Int,
     val countOFQuestions: Int,
     val gameSettings: GameSettings
-)
+): Serializable
